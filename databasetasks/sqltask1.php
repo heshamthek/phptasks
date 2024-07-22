6b64b108cc6d99e39429259c8238886e3fa715a7
heshamthek <hnajem133@gmail.com>
2024-07-22 20:32:22 +0300

    this is done

---
+++ databasetasks/sqltask1.php
@@ -0,0 +1,21 @@
+<!-- A database is an organized collection of structured information, or data, typically stored electronically in a computer system. A database is usually controlled by a database management system (DBMS). Together, the data and the DBMS, along with the applications that are associated with them, are referred to as a database system, often shortened to just database.
+Structured Query Language (SQL) is a standardized programming language that is used to manage relational databases and perform various operations on the data in them.
+SQL is used for the following:
+
+Modifying database table and index structures.
+Adding, updating and deleting rows of data.
+Retrieving subsets of information from within relational database management systems (RDBMSes). This information can be used for transaction processing, analytics applications and other applications that require communicating with a relational database.
+task1 -->
+
+<!-- MySQL is a widely-used, open-source database management system that uses Structured Query Language (SQL) to manage data. Developed by Oracle Corporation, MySQL is known for its speed, reliability, and ease of use.
+key ; Open Source: Free to use and modify, making it accessible to everyone.
+High Performance: Optimized for quick data processing.
+Scalable: Can handle small to very large databases, suitable for many users
+uses
+Web Applications: Powers websites and web apps, especially those using PHP (part of the LAMP stack - Linux, Apache, MySQL, PHP).
+E-Commerce: Manages product catalogs, customer info, and transactions for online stores.
+Content Management Systems (CMS): Used by platforms like WordPress and Drupal to store and manage content.
+
+
+-----------with the task2 pic
+task2 -->
