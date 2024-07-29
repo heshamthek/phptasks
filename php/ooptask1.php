d77154dbb4c9c9e43fcc869ed035c3d4c4633677
heshamthek <hnajem133@gmail.com>
2024-07-29 09:07:28 +0300

    a

--- php/ooptask1.php
+++ php/ooptask1.php
@@ -1,65 +1,58 @@
 <?php
 class Car {
-    private $make;
-    private $model;
-    private $vin;
+    private $make, $model, $vin;
 
     public function __construct($make, $model, $vin) {
         $this->make = $make;
         $this->model = $model;
         $this->vin = $vin;
     }
+
     public function __destruct() {
-        echo "{$this->vin} got destroyed.<br>";
+        echo "{$this->vin} destroyed.<br>";
     }
-    // Get
-    public function getMake() {
-        return $this->make;
+
+    public function getDetails() {
+        return "Make: {$this->make}, Model: {$this->model}, VIN: {$this->vin}";
     }
-    public function getModel() {
-        return $this->model;
-    }
+
     public function getVin() {
         return $this->vin;
     }
-    // Set
-    public function setMake($make) {
-        $this->make = $make;
-    }
-    public function setModel($model) {
-        $this->model = $model;
-    }
-    public function setVin($vin) {
-        $this->vin = $vin;
-    }
-    public function getDetails() {
-        return "make {$this->make}: model {$this->model}: vin {$this->vin}";
-    }
 }
+
 class Inventory {
-    private $cars = array();
+    private $cars = [];
+
     public function addCar($car) {
         $this->cars[] = $car;
     }
+
     public function removeCarByVin($vin) {
         foreach ($this->cars as $index => $car) {
             if ($car->getVin() === $vin) {
                 unset($this->cars[$index]);
                 echo "VIN {$vin} removed.<br>";
+                break;
             }
         }
     }
+
     public function listCars() {
         foreach ($this->cars as $car) {
             echo $car->getDetails() . "<br>";
         }
     }
 }
-$car1 = new Car("hyndai", "avante", "c123");
-$car2 = new Car("bmw", "e46", "a123");
+
+// Example usage
+$car1 = new Car("Hyundai", "Avante", "c123");
+$car2 = new Car("BMW", "E46", "a123");
+
 $inventory = new Inventory();
 $inventory->addCar($car1);
 $inventory->addCar($car2);
+
 $inventory->listCars();
 $inventory->removeCarByVin("c123");
 $inventory->listCars();
